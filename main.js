77d7f558f8b216939313ee6df64b56960ab8b082
beck <beck410@hotmail.com>
2016-03-11 22:02:13 -0600

    set range and domain for x and y axis

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 (function(){
+    config = {
+        'WIDTH': 1000,
+        'HEIGHT': 500,
+        'MARGINS': {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+        }
+    };
 
     function getData() {
         $.getJSON('data.json', function(json) {
@@ -6,8 +16,36 @@
         });
     }
 
+    // range: defines area available to render graph
+    // domain: max and mix values of data
     function buildLineGraph(data) {
-        var salesData = data;
+        var salesData = data,
+            minMaxValues = getMinAndMax(data),
+            vis = d3.select('#visualisation'),
+            xScale = d3.scale.linear().range([
+                config.MARGINS.left,
+                config.WIDTH - config.MARGINS.right
+            ]).domain([
+                salesData[0].year,
+                salesData[salesData.length-1].year
+            ]);
+            yScale = d3.scale.linear().range([
+                config.HEIGHT - config.MARGINS.top,
+                config.MARGINS.bottom
+            ]).domain([
+                minMaxValues.min,
+                minMaxValues.max
+            ]);
+    }
+
+    function getMinAndMax(data) {
+        var salesList = data.map(function(x) {
+            return x.sale;
+        });
+        return {
+            'min': _.min(salesList),
+            'max': _.max(salesList)
+        };
     }
 
     getData();
